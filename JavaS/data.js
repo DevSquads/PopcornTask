bff8c6865be55919da7ce0a707636578b2d362ef
NadaSorour <nadasorourr@gmail.com>
2020-06-10 12:46:08 +0200

    Adding files back it was deleted mistakenly

---
+++ JavaS/data.js
@@ -0,0 +1,14 @@
+function myfun2() { 
+var name = document.getElementById("name").value;
+var pass1 = document.getElementById("pass").value;
+var pass1 = document.getElementById("pass1").value;
+
+if (pass !== pass1 )
+{ 
+   document.getElementById("pass").value = 0;
+}
+    else
+    {
+        alert( "Registered successfully!");
+        
+}
